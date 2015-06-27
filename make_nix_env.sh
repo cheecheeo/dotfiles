226950b3abc3d7b16e6cad9da11f2a318c2f6d1d
John Chee <cheecheeo@gmail.com>
2015-06-27 11:21:45 -0700

    make nix env: prompt for installing graphical packages

--- make_nix_env.sh
+++ make_nix_env.sh
@@ -3,4 +3,13 @@ set -x
 
 mkdir -p $HOME/.nixpkgs/ &&
 cp DOTnixpkgsSLASHconfig.nix $HOME/.nixpkgs/config.nix &&
-nix-env --file $NIXPKGS --install all
+nix-env --file $NIXPKGS --install all &&
+
+echo "Do you wish to install this graphical applications?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes) nix-env --file $NIXPKGS --install all-gui
+             break;;
+        No) break;;
+    esac
+done
