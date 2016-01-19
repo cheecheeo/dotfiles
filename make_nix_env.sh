da7a054cea1d30f84b9c8a66b43c0d5aecb43804
John Chee <cheecheeo@gmail.com>
2016-01-18 19:33:01 -0800

    tighten up nix install script

--- make_nix_env.sh
+++ make_nix_env.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -x
+set -ex
 
 mkdir -p $HOME/.nixpkgs/ &&
 cp DOTnixpkgsSLASHconfig.nix $HOME/.nixpkgs/config.nix &&
 nix-env --file $NIXPKGS --install all &&
 
-echo "Do you wish to install this graphical applications?"
+echo "Do you wish to install this graphical applications?" &&
 select yn in "Yes" "No"; do
     case $yn in
         Yes) nix-env --file $NIXPKGS --install all-gui
