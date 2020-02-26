19ca3ee44499b6b4d891125446a8b4798afa47ed
John Chee <cheecheeo@gmail.com>
2020-02-26 00:32:54 +0000

    add tmux conf and emacs conf to make.sh

--- make.sh
+++ make.sh
@@ -3,4 +3,6 @@
 set -x
 cp DOTgitconfig "$HOME"/.gitconfig
 cp DOTgitignore_global "$HOME"/.gitignore_global
+cp DOTtmux.conf "$HOME"/.tmux.conf
+cp DOTemacs "$HOME"/.emacs
 git config --global core.excludesfile "$HOME"/.gitignore_global
