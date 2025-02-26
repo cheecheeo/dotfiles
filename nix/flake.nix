8fe3387b472f8d3351e9a3c448809e6374d70ffe
John Chee <johnanchee@gmail.com>
2025-02-26 06:52:51 +0000

    nix directory with a flake file (using home-manager)

---
+++ nix/flake.nix
@@ -0,0 +1,51 @@
+# run:
+# nix --extra-experimental-features nix-command --extra-experimental-features flakes flake update
+# sudo nixos-rebuild switch --flake .
+{
+  description = "NixOS configuration with Home Manager and Git";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-wsl.nixosModules.wsl
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nixos = { pkgs, ... }: {
+            home.packages = [ pkgs.git pkgs.neovim pkgs.tmux pkgs.docker ];
+            home.stateVersion = "24.11";
+	    #none of this nvim stuff works for some reason
+            home.sessionVariables.EDITOR = "nvim";
+            programs.neovim = {
+              viAlias = true;
+              vimAlias = true;
+            };
+            programs.git = {
+              enable = true;
+              userName = "John Chee";
+              userEmail = "johnanchee@gmail.com";
+              extraConfig = {
+                github.user = "cheecheeo";
+              };
+            };
+          };
+        }
+      ];
+    };
+  };
+}
